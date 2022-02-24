d4d9847c6dc677ededde2a5ad269063f5a44f3ee
Robson Ferreira <tech.rob@icloud.com>
2022-02-24 13:48:53 -0500

    update main

--- CRUD/main.py
+++ CRUD/main.py
@@ -1,28 +1,18 @@
 import psycopg2
-import sqlalchemy
-from sqlalchemy import create_engine
+from config import config
+# import sqlalchemy
+# from sqlalchemy import create_engine
 
 class Game:
     def __init__(self):
-        self.db_name = 'game'
-        self.db_user = 'rob_the_programmer'
-        self.db_password = input('Enter your password: ')
-        self.db_host = 'localhost'
-        self.db_port = '5432'
+        self.conn = None
+        self.cur = None
+        self.params = config()
 
     def connect(self):
-        '''Connect to the PostgreSQL database server'''
-        self.conn = None
+        '''Connect to the PostGreSQL Database'''
         try:
-            params = {
-                'database': self.db_name,
-                'user': self.db_user,
-                'password': self.db_password,
-                'host': self.db_host,
-                'port': self.db_port
-            }
-            print('Connecting to the PostgreSQL database...')
-            self.conn = psycopg2.connect(**params)
+            self.conn = psycopg2.connect(**self.params)
             self.cur = self.conn.cursor()
             print('PostgreSQL database version:')
             self.cur.execute('SELECT version()')
@@ -31,41 +21,52 @@ class Game:
             self.cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-    
-    def create_tables(self):
-        '''Create tables in the PostgreSQL database'''
-        self.queries = (
-            '''CREATE TABLE IF NOT EXISTS game (
-                id SERIAL PRIMARY KEY,
-                age SMALLINT NOT NULL
-            );'''
-        )
-        self.conn = None
-        try:
-            params = {
-                'database': self.db_name,
-                'user': self.db_user,
-                'password': self.db_password,
-                'host': self.db_host,
-                'port': self.db_port
-            }
-            print('Connecting to the PostgreSQL database...')
-            self.conn = psycopg2.connect(**params)
-            self.cur = self.conn.cursor()
-
-            for query in self.queries:
-                self.cur.execute(query)
-            self.conn.commit()
-            self.cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
         finally:
             if self.conn is not None:
                 self.conn.close()
                 print('Database connection closed.')
 
+    def create_tables(self):
+        '''creates tables in the game database'''
+        queries = (
+            '''
+                CREATE TABLE IF NOT EXISTS  account (
+                userid SERIAL PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                password VARCHAR(50) NOT NULL,
+                email VARCHAR(250) UNIQUE NOT NULL,
+                createdat TIMESTAMP NOT NULL,
+                lastlogin TIMESTAMP);
+            ''',
+            '''
+                CREATE TABLE IF NOT EXISTS job (
+                jobid SERIAL PRIMARY KEY,
+                jobname VARCHAR(200) UNIQUE NOT NULL);
+            ''',
+            '''
+                CREATE TABLE IF NOT EXISTS accountjob (
+                userid INTEGER REFERENCES account(userid),
+                jobid INTEGER REFERENCES job(jobid),
+                hiredate TIMESTAMP);
+            '''
+           ) 
 
+        self.conn = None
+        try:
+            self.conn = psycopg2.connect(**self.params)
+            self.cur = self.conn.cursor()
+            [self.cur.execute(query) for query in queries]
+            self.conn.commit()
+            print('tables created successfully.')
+            self.cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            [self.conn.close() if self.conn is not None else None]
+            
 
 db = Game()
 db.connect()
-# db.create_tables()
+db.create_tables()
+
+        
