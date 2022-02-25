c0745d549d357dcbc3bb05e93c3fd8beead02182
Robson Ferreira <tech.rob@icloud.com>
2022-02-25 10:07:24 -0500

    updates

--- CRUD/main.py
+++ CRUD/main.py
@@ -27,7 +27,7 @@ class Game:
                 print('Database connection closed.')
 
     def create_tables(self):
-        '''creates tables in the game database'''
+        '''creates tables in the database'''
         queries = (
             '''
                 CREATE TABLE IF NOT EXISTS  account (
@@ -63,10 +63,157 @@ class Game:
             print(error)
         finally:
             [self.conn.close() if self.conn is not None else None]
+
+    def create_table(self, newtable, data : dict):
+        '''creates new table in the database'''
+        self.conn = None
+        try:
+            params = config()
+            self.conn = psycopg2.connect(**params)
+            self.cur = self.conn.cursor()
+            query = f"""CREATE TABLE {newtable} ({', '.join([f'{key}' for key in data.keys()])})"""
+            self.cur.execute(query)
+            self.conn.commit()
+            print('new table created successfully.')
+            self.cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            [self.conn.close() if self.conn is not None else None]
+
+    def insert_data(self, table, data : dict):
+        '''inserts data into the database'''
+        self.conn = None
+        try:
+            params = config()
+            self.conn = psycopg2.connect(**params)
+            self.cur = self.conn.cursor()
+            self.cur.execute(f"""INSERT INTO {table} ({','.join(data.keys())}) 
+                                VALUES ({','.join(['%s' for _ in range(len(data))])})""",
+                                tuple(data.values()))
+            self.conn.commit()
+            print('data inserted successfully.')
+            self.cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            [self.conn.close() if self.conn is not None else None]
+
+    def update_data(self, table, data : dict, where : dict):
+        '''updates data in the database'''
+        self.conn = None
+        try:
+            params = config()
+            self.conn = psycopg2.connect(**params)
+            self.cur = self.conn.cursor()
+            self.cur.execute(f"""UPDATE {table} SET {','.join([f'{key}=%s' for key in data.keys()])} 
+                                WHERE {','.join([f'{key}=%s' for key in where.keys()])}""",
+                                tuple(data.values()) + tuple(where.values()))
+            self.conn.commit()
+            print('data updated successfully.')
+            self.cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            [self.conn.close() if self.conn is not None else None]
+
+    def delete_data(self, table, where : dict):
+        '''deletes data from the database'''
+        self.conn = None
+        try:
+            params = config()
+            self.conn = psycopg2.connect(**params)
+            self.cur = self.conn.cursor()
+            self.cur.execute(f"""DELETE FROM {table} 
+                                WHERE {','.join([f'{key}=%s' 
+                                for key in where.keys()])}""",
+                                tuple(where.values()))
+            self.conn.commit()
+            print('data deleted successfully.')
+            self.cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            [self.conn.close() if self.conn is not None else None]
+
+    def alter_table_add(self, table, data : dict):
+        '''alters table by adding data to the database'''
+        self.conn = None
+        try:
+            params = config()
+            self.conn = psycopg2.connect(**params)
+            self.cur = self.conn.cursor()
+            self.cur.execute(f"""ALTER TABLE {table} 
+                                ADD COLUMN {','.join([f'{key}' for key in data.keys()])}""",
+                                tuple(data.values()))
+            self.conn.commit()
+            print('table altered successfully.')
+            self.cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            [self.conn.close() if self.conn is not None else None]
+
+    def alter_table_drop(self, table, data : dict):
+        '''alters table in the database by dropping data'''
+        self.conn = None
+        try:
+            params = config()
+            self.conn = psycopg2.connect(**params)
+            self.cur = self.conn.cursor()
+            self.cur.execute(f"""ALTER TABLE {table} 
+                                DROP COLUMN {','.join([f'{key}' for key in data.keys()])}""",
+                                tuple(data.values()))
+            self.conn.commit()
+            print('table altered successfully.')
+            self.cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            [self.conn.close() if self.conn is not None else None]
+
+    def alter_table_rename(self, table, data : dict):
+        '''alters table in the database by renaming table'''
+        self.conn = None
+        try:
+            params = config()
+            self.conn = psycopg2.connect(**params)
+            self.cur = self.conn.cursor()
+            self.cur.execute(f"""ALTER TABLE {table} RENAME TO {data['newname']}""")
+            self.conn.commit()
+            print('table altered successfully.')
+            self.cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            [self.conn.close() if self.conn is not None else None]
             
 
 db = Game()
 db.connect()
-db.create_tables()
 
+# CREATE 
+# db.create_tables()
+
+# INSERT 
+# db.insert_data('account', {
+#                             'username': 'john', 
+#                             'password': 'johnpass123', 
+#                             'email': 'john@icloud.com',
+#                             'createdat': '2020-01-01',
+#                             'lastlogin': '2020-01-01'
+#                             })
+# db.insert_data('job', {'jobname': 'developer'})
+# db.insert_data('job', {'jobname': 'senior web developer'})
+# db.insert_data('accountjob', {'userid': 1, 'jobid': 1, 'hiredate': '2020-01-01'})
+
+# UPDATE
+# db.update_data('account', {'createdat': 'NOW()'}, {'userid': 1})
+# db.update_data('account', {'lastlogin': 'NOW()'}, {'userid': 1})
+
+# DELETE
+# db.delete_data('job', {'jobid': 2})
+
+# ALTER
+# db.alter_table_rename('information', {'newname': 'information_new'})
         
